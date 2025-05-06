278cb067e831ef34e86fca720c23c2932889aaa9
Alrak-01 <lekanraheem827@gmail.com>
2025-05-06 02:07:12 +0100

    components

---
+++ src/Navbar.jsx
@@ -0,0 +1,17 @@
+const Navbar = () => {
+    return ( 
+        <nav>
+            <h2>Alrak Blog</h2>
+            <ul>
+                <li>
+                    <a href="/create">Home</a>
+                </li>
+                <li>
+                    <a href="/create">New Blog</a>
+                </li>
+            </ul>
+        </nav>
+     );
+}
+ 
+export default Navbar;
