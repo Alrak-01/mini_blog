0ab77f7158f85bd73a4aacdb72651578be567fa2
Alrak-01 <lekanraheem827@gmail.com>
2025-05-15 02:16:01 +0100

    adding tailwind styling to project

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,7 @@
 const Home = () => {
     return ( 
-        <div className="home">
-            <h1>Home Page</h1>
+        <div className="home px-36 pt-10">
+            <h1 className="text-2xl font-semibold">Home Page</h1>
         </div>
      );
 }
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,13 +1,13 @@
 const Navbar = () => {
     return ( 
-        <nav>
-            <h2>Alrak Blog</h2>
-            <ul>
-                <li>
-                    <a href="/create">Home</a>
+        <nav className="flex items-center justify-between px-36 pt-10">
+            <h2 className="text-red-600 text-2xl font-extrabold">Alrak Blog</h2>
+            <ul className="flex items-center justify-between gap-5">
+                <li className="bg-gray-200 hover:bg-red-600 hover:text-white text-sm px-3 py-1 rounded-sm delay-100 duration-300 transition-colors">
+                    <a className="outline-none" href="/create">Home</a>
                 </li>
-                <li>
-                    <a href="/create">New Blog</a>
+                <li className="bg-gray-200 hover:bg-red-600 hover:text-white text-sm px-3 py-1 rounded-sm delay-100 duration-300 transition-colors">
+                    <a className="outline-none" href="/create">New Blog</a>
                 </li>
             </ul>
         </nav>
