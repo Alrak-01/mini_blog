3e0fd2678a4a4841fd82c254f1e2a01897f26cf0
Alrak-01 <lekanraheem827@gmail.com>
2025-05-17 10:03:27 +0100

    using hook filter to create delete

--- src/BlogList.jsx
+++ src/BlogList.jsx
@@ -1,18 +1,15 @@
 import React from 'react'
 
-const BlogList = ({ blogs, title }) => {
-// const blogs = props.blogs;
-// const title = props.title;
-
-
+const BlogList = ({ blogs, title, handleDelete }) => {
 
   return (
-    <div>
+    <div className='blog-list'>
         <p className='font-semibold text-lg pt-5'> {title} </p>
         {blogs.map((blog) => (
             <div className="pt-5 hover:shadow-md delay-100 duration-300 transition ease-in-out p-3 rounded-md" key={blog.id}>
                 <h2 className='text-lg text-red-600 font-semibold'> { blog.title } </h2>
                 <p className=''> { blog.author } </p>
+                <button className='bg-gray-200 rounded-md py-1 px-1 text-sm my-2 hover:bg-red-600 hover:text-white delay-100 duration-300 transition' onClick={() => handleDelete(blog.id)}>Delete Me</button>
             </div>
         ))}
     </div>
--- src/Home.jsx
+++ src/Home.jsx
@@ -4,16 +4,21 @@ import BlogList from './BlogList';
 const Home = () => {
     
     const [blogs, setBlogs] = useState([
-    { title: "Top 10 developers", body: "lorem disco winens....", author: "Walex", id: 1 },
+    { title: "Top 10 developers", body: "lorem disco winens....", author: "James", id: 1 },
     { title: "Web developement core", body: "lorem disco winens....", author: "Cane", id: 2 },
     { title: "React developer incoming", body: "lorem disco winens....", author: "James", id: 3 }
 ]);
 
+    const handleDelete = (id) => {
+    const newBlog = blogs.filter(blog => blog.id !== id);
+    setBlogs(newBlog);
+    
+    };
+
     return ( 
         <div className="home px-36 pt-10">
             <h1 className="text-2xl font-semibold">Home Page</h1>
-           <BlogList blogs={ blogs } title="All Blogs!" />
-            
+           <BlogList blogs={ blogs } title="All Blogs!" handleDelete={handleDelete}/>
         </div>
      );
 }
